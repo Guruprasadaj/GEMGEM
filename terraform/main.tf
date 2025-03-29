a55f50f54926b2f6c89d45037f638c99b2f71c08
Guruprasad Sutar <sutar.guruprasad12@gmail.com>
2025-03-29 18:29:50 +0530

    Add ECR repository configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -328,3 +328,40 @@ resource "random_string" "suffix" {
   special = false
   upper   = false
 }
+
+# Create ECR Repository
+resource "aws_ecr_repository" "app" {
+  name                 = "gemgem"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# Add ECR Repository Policy
+resource "aws_ecr_repository_policy" "app_policy" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowPushPull"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+      }
+    ]
+  })
+}
